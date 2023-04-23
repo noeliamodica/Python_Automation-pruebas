3bd489176378718a4b6754cfe1d0bd1c80b3f523
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-23 19:59:00 -0300

    genero ingreso a google

---
+++ automation.py
@@ -0,0 +1,13 @@
+import pyautogui 
+import time
+
+#time.sleep(6)
+#position = pyautogui.position()
+#print(position)
+
+pyautogui.moveTo(1038, 1055, duration=1)
+pyautogui.click()
+time.sleep(3)
+pyautogui.moveTo(454, 488, duration=1)
+pyautogui.click()
+time.sleep(3)
