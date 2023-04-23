496eb1abbd16dcb435f379ef99ecf2511e1eb1c5
noeliamodica <113479035+noeliamodica@users.noreply.github.com>
2023-04-23 20:08:23 -0300

    automatizo inicio a youtube

--- automation.py
+++ automation.py
@@ -8,6 +8,12 @@ import time
 pyautogui.moveTo(1038, 1055, duration=1)
 pyautogui.click()
 time.sleep(3)
+
 pyautogui.moveTo(454, 488, duration=1)
 pyautogui.click()
 time.sleep(3)
+
+
+pyautogui.typewrite("youtube")
+time.sleep(2)
+pyautogui.hotkey('enter')
